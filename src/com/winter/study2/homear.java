927b52ca8b17ece9cb236530f70fa9df8749c758
gamja44 <https://github.com/gamja44/GDJ83_Java.git>
2024-05-30 05:24:34 +0900

    practice

---
+++ src/com/winter/study2/homear.java
@@ -0,0 +1,140 @@
+package com.winter.study2;
+
+import java.util.Scanner;
+
+public class homear {
+	public static void main(String[] args) {
+		
+//		// 학생 수를 입력 받아서 학생 수 만큼 이름과 국어점수 입력 받아서 최종 출력
+//		// a:80, b:90
+//		Scanner sc = new Scanner(System.in); //키보드 연결하기
+//		
+//		//-----------------------------------------------------------------------------------
+//		System.out.println("학생 수 입력"); //학생 수 입력하기
+//		int count = sc.nextInt(); //count에 학생 수 입력받기
+//		
+//		//내가 입력한 것을 배열에 집어넣을 수 있어야한다
+//		int[] nums =new int[count];
+//		String[] names = new String[count];
+//		int[] kor= new int[count];
+//		
+//		(int i = 0; i < names.length; i++) {
+//			nums[i] = i + 1;
+//			System.out.println("학생 이름 입력");
+//			names[i] = sc.next();
+//			System.out.println(names[i] + "의 국어점수 입력");
+//			kor[i]= sc.nextInt();
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		int[] ar = new int[5];
+		// 각 인덱스 번호 칸에 값을 키보드로 입력받아서 저장하기
+		Scanner sc = new Scanner(System.in); //키보드 연결하기
+		//인덱스 번호 칸에 값을 받기
+		for(int i=0;i<ar.length;i++) {
+		System.out.println(i+1+"번째 값");
+		ar[i]=sc.nextInt();
+		}
+		for (int i=0;i<ar.length;i++) {
+			System.out.println(ar[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
